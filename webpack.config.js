eb8f551a0a02c8a01418089af33cbf7a9c14e376
hchockarprasad <hchockarprasad@gmail.com>
2019-07-16 12:21:55 +0530

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
@@ -7,13 +8,6 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'src/index.html',
-      chunks: ['main']
-    }),
-  ],
   module: {
     rules: [
       {
@@ -24,7 +18,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.(svg|gif|png|eot|woff|ttf)$/,
+        test: /\.(svg|gif|png|jpg|eot|woff|ttf)$/,
         loaders: [
           'url-loader'
         ]
@@ -32,7 +26,36 @@ module.exports = {
       {
         test: /\,js$/,
         loader: 'babel-loader?presets[]=es2015'
+      },
+      {
+        test: /\.(svg|gif|png|jpg)$/,
+        use: {
+          loader: "file-loader"
+        }
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html',
+      chunk: ['main']
+    }), new HtmlWebpackPlugin({
+      filename: 'pricing.html',
+      template: 'src/pricing.html',
+      chunk: ['main']
+    }), new HtmlWebpackPlugin({
+      filename: 'features.html',
+      template: 'src/features.html',
+      chunk: ['main']
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'test.html',
+      template: 'src/test.html',
+      chunk: ['main']
+    }),
+    new CopyWebpackPlugin([
+      {from:'src/img',to:'img'} 
+    ]), 
+  ],
 }
